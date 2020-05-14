9e6ce44688c032e4ad91f5fc7352dc389cdfa041
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-14 19:52:09 +0300

    initial commit

---
+++ src/main/java/sen3004/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package sen3004.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		ProjectApplication.run(SpringApplication.class, args);
+	}
+
+}
