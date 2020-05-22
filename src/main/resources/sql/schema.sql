4601e8aa1112955773fe0d18661f85803e2af93f
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-22 22:09:57 +0300

    defined schema

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE patient(
+    id IDENTITY PRIMARY KEY,
+    tid UNIQUE,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    date_birth DATE,
+    date_symptom DATE,
+    primary_symptom VARCHAR(255),
+);
