43bcfa3f6458ab890925027554ace23553c0a6c8
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-22 22:10:09 +0300

    database population

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO 
+patient(tid, name, surname, date_birth, date_symptom, primary_symptom) 
+VALUES(
+    "97205969546", 
+    "Serdar", 
+    "Al", 
+    parsedatetime("27-11-1998", "dd-MM-yyyy"),
+    parsedatetime("18-05-2020", "dd-MM-yyyy"),
+    "fever"
+);
+INSERT INTO 
+patient(tid, name, surname, date_birth, date_symptom, primary_symptom) 
+VALUES(
+    "55446292052", 
+    "Kaan", 
+    "Biter", 
+    parsedatetime("07-01-1998", "dd-MM-yyyy"),
+    parsedatetime("18-05-2020", "dd-MM-yyyy"),
+    "diarrhoea"
+);
