62f7e6e9e4eac9ce5c2ff07f3a4d2ccf479bde1d
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-23 00:06:31 +0300

    fixed type in patient table

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE patient(
     id IDENTITY PRIMARY KEY,
-    tid UNIQUE,
+    tid BIGINT UNIQUE,
     name VARCHAR(255),
     surname VARCHAR(255),
     date_birth DATE,
     date_symptom DATE,
-    primary_symptom VARCHAR(255),
+    primary_symptom VARCHAR(255)
 );
