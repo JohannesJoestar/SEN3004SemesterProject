31d65157bf4de838503c6a974740810e05c2725c
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-26 02:47:08 +0300

    integrated repository to service

--- src/main/java/sen3004/project/dao/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package sen3004.project.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import sen3004.project.model.Patient;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {}
