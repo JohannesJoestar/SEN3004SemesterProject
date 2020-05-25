64751fe93eba58023e9247e0989d971ec9c4377d
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-26 02:22:44 +0300

    schema changes for multiple symptoms

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,20 +1,35 @@
 INSERT INTO 
-patient(tid, name, surname, date_birth, date_symptom, primary_symptom) 
-VALUES(
+patient(id, tid, name, surname, date_birth, date_symptom, primary_symptom) 
+VALUES (
+    1,
     97205969546, 
     'Serdar', 
     'Al', 
     parsedatetime('27-11-1998', 'dd-MM-yyyy'),
-    parsedatetime('18-05-2020', 'dd-MM-yyyy'),
-    'fever'
+    parsedatetime('18-05-2020', 'dd-MM-yyyy')
 );
 INSERT INTO 
-patient(tid, name, surname, date_birth, date_symptom, primary_symptom) 
-VALUES(
+patient(id, tid, name, surname, date_birth, date_symptom, primary_symptom) 
+VALUES (
+    2,
     55446292052, 
     'Kaan', 
     'Biter', 
     parsedatetime('07-01-1998', 'dd-MM-yyyy'),
-    parsedatetime('18-05-2020', 'dd-MM-yyyy'),
-    'diarrhoea'
+    parsedatetime('18-05-2020', 'dd-MM-yyyy')
 );
+
+INSERT INTO symptom(id, key) VALUES (1, "fever");
+INSERT INTO symptom(id, key) VALUES (2, "dry_cough");
+INSERT INTO symptom(id, key) VALUES (3, "tiredness");
+INSERT INTO symptom(id, key) VALUES (4, "diarrhoea");
+INSERT INTO symptom(id, key) VALUES (5, "cant_breathe");
+INSERT INTO symptom(id, key) VALUES (6, "taste_sense_loss");
+INSERT INTO symptom(id, key) VALUES (7, "chest_pain");
+
+INSERT INTO patient_symptom(pid, sid) VALUES (1, 4);
+INSERT INTO patient_symptom(pid, sid) VALUES (1, 7);
+INSERT INTO patient_symptom(pid, sid) VALUES (1, 2);
+INSERT INTO patient_symptom(pid, sid) VALUES (1, 6);
+INSERT INTO patient_symptom(pid, sid) VALUES (2, 5);
+INSERT INTO patient_symptom(pid, sid) VALUES (2, 4);
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -4,6 +4,17 @@ CREATE TABLE patient(
     name VARCHAR(255),
     surname VARCHAR(255),
     date_birth DATE,
-    date_symptom DATE,
-    primary_symptom VARCHAR(255)
+    date_symptom DATE
+);
+
+CREATE TABLE symptom(
+    id IDENTITY PRIMARY KEY,
+    key VARCHAR(100) 
+);
+
+CREATE TABLE patient_symptom(
+    pid BIGINT,
+    sid BIGINT,
+    FOREIGN KEY(pid) REFERENCES patient(id),
+    FOREIGN KEY(sid) REFERENCES symptom(id)
 );
